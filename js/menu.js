e76eb4b26be1e9a45cdfab43da485d4da61bb864
Ahmed Mukhtar Muhammed <ahmmuh@MacBook-Air-som-tillhor-Ahmed.local>
2021-01-05 00:08:57 +0100

    added a little jquery in menujs

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,15 @@
 $(function () {
-  console.log("ok");
+  var cardCol = $(".header-boxes .row .col  ");
+
+  $(cardCol).mouseover(function () {
+    $(cardCol).addClass("animate__animated animate__pulse");
+  });
+
+  var gallery = $(".img-fluid");
+  $.each(gallery, function (i, image) {
+    var newImage = image;
+    $(newImage).mouseover(function () {
+      $(newImage).addClass("animate__animated animate__pulse");
+    });
+  });
 });
